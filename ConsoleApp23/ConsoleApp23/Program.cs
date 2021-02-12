9ae99c41dfeaac1de7ae1064e668c08bb7d540b6
VilgotPersson <69978046+VilgotPersson@users.noreply.github.com>
2021-02-12 09:39:16 +0100

    Add files via upload

---
+++ ConsoleApp23/ConsoleApp23/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Insertionsort(int[] minlista)
+        {
+            int i, n;
+            int length = minlista.Length;
+            if (length < 2) return;
+            int temp = 0;
+            for (n = 1; n < length; n++)
+            {
+                temp = minlista[n];
+                i = n - 1;
+                while(i>=0 && minlista[i]>temp)
+                {
+                    minlista[i + 1] = minlista[i];
+                    i--;
+                }
+                minlista[i + 1] = temp;
+            }
+        }
+        /*static void Bubbelsort(List<int> minLista)
+        {
+            for(int i = 0; i < minLista.Count; i++)
+            {
+                for(int j=0; j < minLista.Count-1; j++)
+                {
+                    if(minLista[j] > minLista[j+1])
+                    {
+                        int tempo = minLista[j];
+                        minLista[j] = minLista[j + 1];
+                        minLista[j + 1] = tempo;
+                    }
+                }
+            }
+                
+        }
+        */
+        static void Main(string[] args)
+        {
+
+
+            var tallista = new List<int>();
+            Random slump = new Random();
+            for(int i = 0; i < 10000; i++)
+            {
+                tallista.Add(slump.Next(100000));
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Insertionsort(tallista);
+            stopWatch.Stop();
+            Console.WriteLine("Listan är sorterad!");
+            Console.WriteLine("Tiden: " + stopWatch.Elapsed);
+            }
+
+        private static void Insertionsort(List<int> tallista)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
